4526a63b96b9f8152fc1bae081909d9145dcd524
Larry_A <larryagbana@gmail.com>
2020-07-23 11:19:35 -0400

    login and sign up navbar

---
+++ client/src/components/AltMenu/index.js
@@ -0,0 +1,52 @@
+// Import React dependencies
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+// Import Menu and SubMenu components from ant design
+import { Menu } from 'antd'
+
+
+const styling = {
+  menu: {
+    backgroundColor: '#353452'
+  },
+  item: {
+    color: '#ffffff',
+    fontFamily: 'Poppins',
+    fontSize: '1rem'
+  }
+}
+
+// Right Menu component
+function AltMenu (props) {
+  const { order } = props
+  const [current, setCurrent] = useState('mail')
+
+  const handleClick = e => {
+    // console.log('click ', e)
+    setCurrent({ current: e.key })
+  }
+
+  // Function to scroll to the top of the page when called
+  // calling react-scroll method
+
+  return (
+    <Menu
+      onClick={handleClick}
+      selectedKeys={[current]}
+      mode={order}
+      style={styling.menu}
+    >
+      <Menu.Item key='signup'>
+        <Link to='/signup' style={styling.item}>
+          Sign Up
+        </Link>
+      </Menu.Item>
+      <Menu.Item key='login'>
+        <Link to='/login' style={styling.item}>
+          Log In
+        </Link>
+      </Menu.Item>
+    </Menu>
+  )
+}
+export default AltMenu
